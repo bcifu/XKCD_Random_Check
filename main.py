8ca3335ea09f0e3f424a47572af1781b9397cd4a
Ben Cifu <bcifu@ucls.uchicago.edu>
2020-01-03 17:21:18 -0600

    Add in a progress bar

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ import pickle
 import os
 import argparse
 import random
+from progress.bar import Bar
 
 parser = argparse.ArgumentParser(description= "get information on XKCD random for all comics a specefied number of times")
 parser.add_argument('--repeat_times', '-r', default=2)
 repeat = parser.parse_args().repeat_times
-
 
 #Load any past data
 if os.path.isfile('data.pickle'):
     data = pickle.load(open('data.pickle', 'rb'))
 else:
     data = {}
-
-print(data)
 
 #Data is structured with the keys as the starting comment and each value in its dict as a redirect instance
 
@@ -37,7 +35,7 @@ for repeatCount in range(repeat):
     comicList.extend(range(1, numberOfComics + 1))
 random.shuffle(comicList)
 
-for comicNum in comicList:
+for comicNum in Bar('Checking').iter(comicList):
     driver.get('https://xkcd.com/{}/'.format(comicNum))
     driver.find_element_by_xpath('/html/body/div[2]/ul[1]/li[3]/a').click()
     newNumber = int(re.search(r'\d+', driver.current_url).group(0))
@@ -52,7 +50,5 @@ if os.path.isfile('data.pickle'):
     os.remove('data.pickle')
 
 os.rename('newsave.pickle', 'data.pickle')
-
-print(data)
 
 driver.close()
